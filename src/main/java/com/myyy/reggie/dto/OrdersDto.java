f35e6c63b69f76d08b7a056ac4acd555043b6357
Myy <1452265235@qq.com>
2024-07-08 16:12:55 +0800

    菜品管理（删除、批量删除、起售停售)、套餐管理（修改、起售停售）、订单明细

--- src/main/java/com/myyy/reggie/dto/OrdersDto.java
+++ src/main/java/com/myyy/reggie/dto/OrdersDto.java
@@ -2,10 +2,11 @@ package com.myyy.reggie.dto;
 
 import com.myyy.reggie.entity.OrderDetail;
 import com.myyy.reggie.entity.Orders;
-
+import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class OrdersDto extends Orders {
 
     private List<OrderDetail> orderDetails = new ArrayList<>();
